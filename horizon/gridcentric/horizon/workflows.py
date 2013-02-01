b0032dcefd4886a41148623bd433b60cc298dff7
Trevor Keith <tkeith@gridcentric.ca>
2013-02-01 10:44:14 -0500

    Added GC migration support to Horizon
    Change-Id: Iaa61c956db8dad4997e5487df64182c7a8edb174

--- horizon/gridcentric/horizon/workflows.py
+++ horizon/gridcentric/horizon/workflows.py
@@ -86,3 +86,56 @@ class LaunchBlessed(workflows.Workflow):
         except:
             exceptions.handle(request)
             return False
+
+class GCMigrateAction(workflows.Action):
+    dest_id = forms.DynamicChoiceField(label=_("Destination Host"),
+                                       required=False)
+
+    class Meta:
+        name = _("Migrate")
+        help_text = _("")
+
+    def populate_dest_id_choices(self, request, context):
+        gc_hosts = api.list_gc_hosts(request)
+        try:
+            hosts = [(host.host_name, host.host_name) for host in gc_hosts]
+        except:
+            hosts = []
+            exceptions.handle(request,
+                              _('Unable to retrieve hosts.'))
+        if hosts:
+            hosts.insert(0, ("", _("Automatically select")))
+        else:
+            hosts = (("", _("No hosts available.")),)
+        return hosts
+
+class GCMigrateStep(workflows.Step):
+    action_class = GCMigrateAction
+    depends_on = ('instance_id',)
+    contributes = ('dest_id',)
+
+    def contribute(self, data, context):
+        if data:
+            post = self.workflow.request.POST
+            context['dest_id'] = post['dest_id'] or None
+        return context
+
+class GCMigrate(workflows.Workflow):
+    slug = 'gc_migrate'
+    name = _("Migrate")
+    finalize_button_name = _("Migrate")
+    success_message = _("Instance migration has been initiated")
+    failure_message = _("Unable to initiate migration")
+    success_url = "horizon:nova:instances:index"
+    default_steps = (GCMigrateStep,)
+
+    def format_status_message(self, message):
+        return message
+
+    def handle(self, request, context):
+        try:
+            api.gc_migrate(request, context['instance_id'], context['dest_id'])
+            return True
+        except:
+            exceptions.handle(request)
+            return False
