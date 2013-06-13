gridcentric/cobalt

commit 7c1f27b67d53c97ef0eb417564ba16df70db1979

don't download _base image on launch
Change-Id: I6bed4511f0bd035bfa87f562e728a3be8049ebd9

cobalt/nova/extension/vmsconn.py
@@ -31,7 +31,8 @@ from nova import exception
 
 from nova.virt import images
 from nova.virt.libvirt import blockinfo
-from nova .virt.libvirt import imagebackend
+from nova.virt.libvirt import imagebackend
+from nova.virt.libvirt.imagecache import get_cache_fname
 from nova.virt.libvirt import utils as libvirt_utils
 from nova.compute import utils as compute_utils
 from nova.openstack.common import log as logging
@@ -475,15 +476,15 @@ class LibvirtConnection(VmsConnection):
                    image_refs=[],
                    lvm_info={}):
 
-        image_base_path = None
+        image_base_path = os.path.join(CONF.instances_path, CONF.base_dir_name)
+        if not os.path.exists(image_base_path):
+            LOG.debug('Base path %s does not exist. It will be created now.', image_base_path)
+            mkdir_as(image_base_path, self.openstack_uid)
+
         if not(skip_image_service) and CONF.cobalt_use_image_service:
             # We need to first download the descriptor and the disk files
             # from the image service.
             LOG.debug("Downloading images %s from the image service." % (image_refs))
-            image_base_path = os.path.join(CONF.instances_path, '_base')
-            if not os.path.exists(image_base_path):
-                LOG.debug('Base path %s does not exist. It will be created now.', image_base_path)
-                mkdir_as(image_base_path, self.openstack_uid)
 
             for image_ref in image_refs:
                 image = self.image_service.show(context, image_ref)
@@ -555,6 +556,21 @@ class LibvirtConnection(VmsConnection):
         for network_ref, mapping in network_info:
             network_ref['injected'] = False
 
+        # Stub out an image in the _base directory so libvirt_conn._create_image
+        # doesn't try to download the base image that the master was booted
+        # from, which isn't necessary because a clone's thin-provisioned disk is
+        # overlaid on a disk image accessible via VMS_DISK_URL. Note that we
+        # can't just touch the image's file in _base because master instances
+        # will need the real _base data. So we trick _create_image into using
+        # some bogus id for the image id that'll never be the id of a real
+        # image; the new instance's uuid suffices.
+        disk_images = {'image_id': new_instance_ref['uuid'],
+                       'kernel_id': new_instance_ref['kernel_id'],
+                       'ramdisk_id': new_instance_ref['ramdisk_id']}
+        touch_as(os.path.join(image_base_path,
+                              get_cache_fname(disk_images, 'image_id')),
+                 self.openstack_uid)
+
         # (dscannell) This was taken from the core nova project as part of the
         # boot path for normal instances. We basically want to mimic this
         # functionality.
@@ -568,12 +584,14 @@ class LibvirtConnection(VmsConnection):
             libvirt_conn._create_image(context, instance_dict, xml,
                                        disk_info['mapping'],
                                        network_info=network_info,
-                                       block_device_info=block_device_info)
+                                       block_device_info=block_device_info,
+                                       disk_images=disk_images)
         else:
             libvirt_conn._create_image(context, instance_dict,
                                        disk_info['mapping'],
                                        network_info=network_info,
-                                       block_device_info=block_device_info)
+                                       block_device_info=block_device_info,
+                                       disk_images=disk_images)
             xml = libvirt_conn.to_xml(instance_dict, network_info, disk_info,
                                       block_device_info=block_device_info,
                                       write_to_disk=True)

commit bc01491d296121fccb34149d0fc2fe9938f9b322

fix launch: path=None unless using image service
A bug was introduced in 7c1f27b67d53c97ef0eb417564ba16df70db1979
wherein the base_ image directory was always given as the path
argument to to vms.management.Management.launch, thereby overriding
VMS_DISK_URL. Since the path argument to launch tells it where to look
for artifacts (descriptor files, base disks, etc.), we only want to
set it to nova's base_ directory when we're using glance to store
bless artifacts.

Change-Id: I59261b4c99f03fb2c129077d9e5b7538bd547b42

cobalt/nova/extension/vmsconn.py
@@ -518,7 +518,9 @@ class LibvirtConnection(VmsConnection):
             LOG.debug('Base path %s does not exist. It will be created now.', image_base_path)
             mkdir_as(image_base_path, self.openstack_uid)
 
+        artifact_path = None
         if not(skip_image_service) and CONF.cobalt_use_image_service:
+            artifact_path = image_base_path
             # We need to first download the descriptor and the disk files
             # from the image service.
             LOG.debug("Downloading images %s from the image service." % (image_refs))
@@ -651,7 +653,7 @@ class LibvirtConnection(VmsConnection):
         # Return the libvirt file, this will be passed in as the name. This
         # parameter is overloaded in the management interface as a libvirt
         # special case.
-        return (libvirt_file, image_base_path)
+        return (libvirt_file, artifact_path)
 
     @_log_call
     def post_launch(self, context,